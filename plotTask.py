e1271ad9de330b87afdd3a0022c6916eae516936
katemcg93 <katemcg93@gmail.com>
2021-03-14 21:27:52 +0000

    Updates to Wk 8

--- plotTask.py
+++ plotTask.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-straightLineX = np.array(range(0,4))
+straightLineX = np.array(range(0,5))
 straightLineY = straightLineX*1
 
+squaredX = np.array(range(0,4))
+squaredY = squaredX * squaredX
+
+cubedX = np.array(range(0,4))
+cubedY = cubedX *cubedX* cubedX
+
+
+
 plt.plot(straightLineX, straightLineY)
+plt.plot(squaredX, squaredY)
+plt.plot(cubedX, cubedY)
 
 plt.show()
