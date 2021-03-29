99e66f1ce250ca87b073228a32a3aea53c3f994d
katemcg93 <katemcg93@gmail.com>
2021-03-29 20:44:34 +0100

    changing tick and legend font on plot

--- plotTask.py
+++ plotTask.py
@@ -23,9 +23,9 @@ plt.plot(cubedX, cubedY, label = "f$(x^3)$", linewidth = 3, linestyle = "dashed"
 plt.subplots_adjust(left = 0.098, bottom = 0.24,  right = 0.952)
 
 #Title and axis labels
-plt.title(label = "Function Graph", pad = 2, linespacing = 2, fontsize = 30, color = "#d2cfd3")
-plt.xlabel("x axis")
-plt.ylabel("y axis")
+plt.title(label = "Function Graph", pad = 2, linespacing = 2, fontsize = 20, color = "#978a84")
+plt.xlabel("x axis", color = "#978a84")
+plt.ylabel("y axis", color = "#978a84")
 
 #making plot slightly wider 
 plt.subplots_adjust(left = 0.098, bottom = 0.24,  right = 0.952)
@@ -33,10 +33,17 @@ plt.subplots_adjust(left = 0.098, bottom = 0.24,  right = 0.952)
 #Changing x and y axis color to match title, hiding top and right spines
 ax = plt.axes()
 
-ax.spines["bottom"].set_color("#d2cfd3")
-ax.spines["left"].set_color("#d2cfd3")
+ax.spines["bottom"].set_color("#978a84")
+ax.spines["left"].set_color("#978a84")
 ax.spines["top"].set_color("none")
 ax.spines["right"].set_color("none")
 
-plt.legend()
+#setting ticks on axis to match spine color
+ax.tick_params(labelcolor = "#d2cfd3", color ="#978a84")
+
+
+#Changing font color on legend labels
+legend = plt.legend()
+plt.setp(legend.get_texts(), color = "#978a84")
+
 plt.show()
